2c0cd1bb0927d0481331fc0e026b9727cbf665dd
Hooman Mohammadi <h.mohammadi.f@gmail.com>
2022-10-10 20:43:28 +0200

    Login and Register pages have been added.

---
+++ BlazorBattles/BlazorBattles/Shared/UserLogin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Shared
+{
+    public class UserLogin
+    {
+        [Required(ErrorMessage ="Please enter a valid user name.")]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
